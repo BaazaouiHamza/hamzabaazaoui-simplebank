6491c19421720164a8a53d12bc6b9894e6319ceb
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-05-13 20:24:24 +0100

    Add users table

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -21,7 +21,7 @@ CREATE TABLE "transfers" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE UNIQUE INDEX ON "accounts" ("owner");
+CREATE INDEX ON "accounts" ("owner");
 
 CREATE INDEX ON "entries" ("account_id");
 
