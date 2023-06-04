fd681142254a6bdf3afedc1af70e044bcfe9b46a
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-06-04 14:57:53 +0100

    implement login user API that returns PASETO or JWT access token

--- api/server.go
+++ api/server.go
@@ -1,37 +1,63 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/hamzabaazaoui/simplebank/db/sqlc"
+	"github.com/hamzabaazaoui/simplebank/token"
+	"github.com/hamzabaazaoui/simplebank/util"
 )
 
 // Server serves HTTP requests for our banking services
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	router     *gin.Engine
+	tokenMaker token.Maker
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+
+	// you can either a JWTToken or a PasetToken
+	// tokenMaker, err := token.NewJWTMaker(config.TokenSymetricKey)
+
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		store:      store,
+		tokenMaker: tokenMaker,
+		config:     config,
+	}
 
 	// add routes to router
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setUpRouter()
+	return server, nil
+}
+
+func (server *Server) setUpRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+
 	router.POST("/transfers", server.createTransfer)
-	router.POST("/users", server.createUser)
 
 	server.router = router
 
-	return server
 }
 
 // Start runs the HTTP server on a specific address.
--- go.mod
+++ go.mod
@@ -4,13 +4,15 @@ go 1.20
 
 require github.com/lib/pq v1.10.7
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/o1egl/paseto v1.0.0
+)
 
 require (
 	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
 	github.com/aead/chacha20poly1305 v0.0.0-20170617001512-233f39982aeb // indirect
 	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
-	github.com/o1egl/paseto v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 )
 
